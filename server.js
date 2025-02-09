879658e298d8a72054a8d054b82877b2e07ff1d1
ᴛᴀᴋᴀʜɪʀᴏᴇx666 <105994154+TakahiroEX666@users.noreply.github.com>
2025-02-09 12:19:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,30 +6,14 @@ app.use(express.json());
 let lastMessage = null; // ตัวแปรเก็บข้อความล่าสุด
 
 // Endpoint สำหรับรับข้อความ (POST)
-app.post("/toalioth", (req, res) => {
-    lastMessage = req.body.message; // บันทึกข้อความล่าสุด
-    console.log("Received Message:", lastMessage);
-    res.send({ status: "OK", received: lastMessage });
-});
-
-app.post("/tocupid", (req, res) => {
+app.post("/post", (req, res) => {
     lastMessage = req.body.message; // บันทึกข้อความล่าสุด
     console.log("Received Message:", lastMessage);
     res.send({ status: "OK", received: lastMessage });
 });
 
 
-app.get("/get-cupid", (req, res) => {
-    if (lastMessage) {
-        const messageToSend = lastMessage; // เก็บข้อความก่อนลบ
-        lastMessage = null; // ลบข้อความหลังส่ง
-        res.send({ message: messageToSend });
-    } else {
-        res.send({ message: "❌" });
-    }
-});
-
-app.get("/get-alioth", (req, res) => {
+app.get("/get", (req, res) => {
     if (lastMessage) {
         const messageToSend = lastMessage; // เก็บข้อความก่อนลบ
         lastMessage = null; // ลบข้อความหลังส่ง
