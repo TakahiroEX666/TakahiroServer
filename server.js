4aee9822775eef42df851b0159ac7aec3cef585e
ᴛᴀᴋᴀʜɪʀᴏᴇx666 <105994154+TakahiroEX666@users.noreply.github.com>
2025-02-10 05:35:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,35 +6,36 @@ app.use(express.json());
 let AliothMessage = null;
 let CupidMessage = null;
 
-
-app.get("/server", (req, res) => {res.send({ message: "Server Running" });
+app.get("/server", (req, res) => {
+    res.send({ message: "Server Running" });
+});
 
 app.post("/toalioth", (req, res) => {
-   AliothMessage = req.body.message;
-   res.send({ status: "OK", received: AliothMessage });
+    AliothMessage = req.body.message;
+    res.send({ status: "OK", received: AliothMessage });
 });
 
 app.post("/tocupid", (req, res) => {
-   CupidMessage = req.body.message;
-   res.send({ status: "OK", received: AliothMessage });
+    CupidMessage = req.body.message;
+    res.send({ status: "OK", received: CupidMessage });
 });
 
-app.get("/getalioth", (req, red) => {
-   if(AliothMessage) {
-      res.send({ message: AliothMessage });
-      AliothMessage = null;
-   } else {
-      res.send({ message: "❌" });
-   }
+app.get("/getalioth", (req, res) => {
+    if (AliothMessage) {
+        res.send({ message: AliothMessage });
+        AliothMessage = null;
+    } else {
+        res.send({ message: "❌" });
+    }
 });
 
-app.get("/getcupid", (req, red) => {
-   if(CupidMessage) {
-      res.send({ message: CupidMessage });
-      CupidMessage = null;
-   } else {
-      res.send({ message: "❌" });
-   }
+app.get("/getcupid", (req, res) => {
+    if (CupidMessage) {
+        res.send({ message: CupidMessage });
+        CupidMessage = null;
+    } else {
+        res.send({ message: "❌" });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
