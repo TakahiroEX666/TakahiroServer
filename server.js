1cb3c7cb8141d4626121332231fe3681068bf734
ᴛᴀᴋᴀʜɪʀᴏᴇx666 <105994154+TakahiroEX666@users.noreply.github.com>
2025-02-09 07:44:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,36 @@ app.use(express.json());
 let lastMessage = null; // ตัวแปรเก็บข้อความล่าสุด
 
 // Endpoint สำหรับรับข้อความ (POST)
-app.post("/webhook", (req, res) => {
+app.post("/toalioth", (req, res) => {
     lastMessage = req.body.message; // บันทึกข้อความล่าสุด
     console.log("Received Message:", lastMessage);
     res.send({ status: "OK", received: lastMessage });
 });
 
-// Endpoint สำหรับ GET แล้วลบทันที
-app.get("/get-message", (req, res) => {
+app.post("/tocupid", (req, res) => {
+    lastMessage = req.body.message; // บันทึกข้อความล่าสุด
+    console.log("Received Message:", lastMessage);
+    res.send({ status: "OK", received: lastMessage });
+});
+
+
+app.get("/get-cupid", (req, res) => {
     if (lastMessage) {
         const messageToSend = lastMessage; // เก็บข้อความก่อนลบ
         lastMessage = null; // ลบข้อความหลังส่ง
         res.send({ message: messageToSend });
     } else {
-        res.send({ message: "No new messages" });
+        res.send({ message: "❌" });
+    }
+});
+
+app.get("/get-alioth", (req, res) => {
+    if (lastMessage) {
+        const messageToSend = lastMessage; // เก็บข้อความก่อนลบ
+        lastMessage = null; // ลบข้อความหลังส่ง
+        res.send({ message: messageToSend });
+    } else {
+        res.send({ message: "❌" });
     }
 });
 
