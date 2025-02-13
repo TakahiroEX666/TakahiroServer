a718aac7cc32552e034b69f95abedfebcdd4bf17
ᴛᴀᴋᴀʜɪʀᴏᴇx666 <105994154+TakahiroEX666@users.noreply.github.com>
2025-02-13 07:14:11 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
-$('.regione').on('click',function(){
-  $('.regione').removeClass('selected');
-  $(this).addClass('selected'); 
-  console.log($(this).data('nome-regione')); 
+$(document).ready(function () {
+    $(".regione").on("click", function () {
+        let regionName = $(this).data("nome-regione");
+
+        // เพิ่มชื่อของเขตที่คลิกเข้าไปในรายการ
+        let labelContainer = $("#region-names");
+        labelContainer.append(`<div class="region-label">${regionName}</div>`);
+    });
 });
