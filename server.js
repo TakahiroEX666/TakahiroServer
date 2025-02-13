2d01cc9f616c1082a54ce29a92d9ff5e58390181
ᴛᴀᴋᴀʜɪʀᴏᴇx666 <105994154+TakahiroEX666@users.noreply.github.com>
2025-02-13 22:09:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -68,12 +68,12 @@ app.get("/getalioth", (req, res) => {
 //getCupid
 app.get("/getcupid", (req, res) => {
     res.send(CupidMessage);
-   // if (CupidMessage!=null) {
-    //    res.send(CupidMessage);
-    //    CupidMessage = null;
-  //  } else {
-     //   res.send({ message: "❌" });
-   // }
+    if (CupidMessage!=null) {
+        res.send(CupidMessage);
+        CupidMessage = null;
+    } else {
+        res.send({ message: "❌" });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
